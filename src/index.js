3ee9b5db491f64a149c54b106822f6518207e85a
JunyoungOh <47840083+JunyoungOh@users.noreply.github.com>
2025-09-19 10:34:47 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 // src/index.js
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css'; // 전역 스타일이 있다면 유지하세요.
+import './index.css';
 import App from './App';
+import { ToastHost } from './components/ui/Toast';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -10,5 +11,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
+    {/* 전역 토스트 호스트: 루트에서 단 한 번만 렌더 */}
+    <ToastHost />
   </React.StrictMode>
 );
